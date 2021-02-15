fab1b5e43d9aff0e32e3e9a003dde3289b573e5a
Andersson Quintero <anderssonq@gmail.com>
2021-02-15 01:23:12 -0400

    🔇 logs rmeoved and comments usseless deleted

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -38,23 +38,3 @@ export const apiHttp = async (method, endpoint, data, options = {}, loading = tr
   window.getApp.$emit('LOADING', false);
   return serviceResponse;
 };
-// function buildErrorMessage (error) {
-//   console.error(error.response)
-//   const errorResponse = {
-//     ok: 0,
-//     message: {
-//       code: String,
-//       text: String,
-//     },
-//   }
-//   if (typeof error.response === 'undefined') { errorResponse.message.text =
-// i18n.t('message.apiErrorUndefined') } else if (error.response.status === 404)
-// { errorResponse.message.text = i18n.t('message.apiError404') } else if (error.response.status === 500)
-//  { errorResponse.message.text = i18n.t('message.apiError500') } else if
-//  (error.response.status === 405 || error.response.status === 406)
-//  { errorResponse.message.text = i18n.t('message.apiError405_406') }
-//   else errorResponse.message.text = error.response.data.message[0]
-//   errorResponse.ok = 0
-//   errorResponse.message.code = 'E999'
-//   return errorResponse
-// }
